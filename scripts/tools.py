6052efa0d3afc658035c0043e0c82a37bf0efe58
stefp <stefano.puliti@gmail.com>
2022-05-23 11:19:33 +0200

    Update tools.py

--- scripts/tools.py
+++ scripts/tools.py
@@ -122,7 +122,7 @@ def tile_ortho(ortho_path, tile_size_m, buffer_size_m, format_tiles):
 # arguments: 
 # - png_dir= directory where png tiles (20 m side) are stored
 ##########################################################################################################################################################################
-def predict_wheelRuts(png_dir, model_dir):
+def predict_wheelRuts(png_dir, model_dir, model_name):
     
     # define output path
     output_dir= png_dir+'/predictions'
@@ -132,10 +132,10 @@ def predict_wheelRuts(png_dir, model_dir):
         os.makedirs(output_dir)
         
     #os.chdir(model_dir)
-    print("Model directory ................................"+model_dir+ "resnet_unet_rgb_20mpatch_all")
+    print("Model directory ................................"+model_dir+ model_name)
     # run inference
     predict_multiple( 
-      checkpoints_path=model_dir + "resnet_unet_rgb_20mpatch_all" , #path to weights (stored model .json file)
+      checkpoints_path=model_dir + model_name , #path to weights (stored model .json file)
       inp_dir=png_dir , #path to files to be predicted (.png images)
       out_dir=output_dir #path to predicted files - would be in .png format
     )
