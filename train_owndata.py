9b1a0644119f9a52a480c227b110f13f40dfd737
Stefano Puliti <stefano.puliti@gmail.com>
2022-06-08 13:41:30 +0200

    Update train_owndata.py

--- train_owndata.py
+++ train_owndata.py
@@ -2,7 +2,7 @@
 
 from keras_segmentation.models.unet import resnet50_unet #for running other pretrained architectures, please refer introduction of the repository
 #number of classes #no. of class is 'no. of labels + 1' as NaN values was given label ==0;
-n = 3
+n = 2
 
 model = resnet50_unet(n_classes=n ,  input_height=1024, input_width=1024  ) #input height, width is adjustable based on the area under consideration
 #recommendation: 1024 x 1024 
